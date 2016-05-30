bd53aecb166c06f22809d4b5491594c59bbbc8ae
ioanungurean <inungurean@gmail.com>
2016-05-31 01:57:22 +0300

    Toast delay updated

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -17,7 +17,7 @@ function MainCtrl($mdSidenav, $mdToast) {
 	};
 
 	vm.toast = function(message) {
-		var toast = $mdToast.simple().content('You clicked ' + message).position('bottom right');
+		var toast = $mdToast.simple().content('You clicked ' + message).position('bottom right').hideDelay(300);
 		$mdToast.show(toast);
 	};
 
@@ -25,9 +25,9 @@ function MainCtrl($mdSidenav, $mdToast) {
 		vm.theme = theme;
 		$mdToast.show(
 			$mdToast.simple()
+			.hideDelay(300)
 			.content('Your theme is now ' + theme)
 			.position('bottom right')
-			.hideDelay(500)
 		);
 		localStorage.setItem('theme', theme);
 	}
