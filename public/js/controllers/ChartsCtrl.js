ec44c520144fd4a3686b762a65fb7f3c7f061ef0
Mihaela Radu <mihaelaradum@gmail.com>
2016-06-07 10:04:18 +0300

    FInished first chart

--- public/js/controllers/ChartsCtrl.js
+++ public/js/controllers/ChartsCtrl.js
@@ -11,10 +11,25 @@ function ChartsCtrl($scope, countryService) {
                     "Mean Years of Schooling",
                     "Estimated Gross Income" ];
 
+  $scope.selectedCountry;
+
   $scope.data = [
-    [65, 59, 90, 81, 56],
-    [28, 48, 40, 19, 96]
-  ];
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0]
+    ];
+
+  $scope.getSelectedCountry = function() {
+    if ($scope.selectedCountry !== undefined) {
+    $scope.data = [
+      [$scope.selectedCountry.hdi_m/0.945, $scope.selectedCountry.le_m/81.2, $scope.selectedCountry.eys_m/16.1, $scope.selectedCountry.mys_m/13.8, $scope.selectedCountry.gni_m/143979],
+      [$scope.selectedCountry.hdi_f/0.94, $scope.selectedCountry.le_f/86.8, $scope.selectedCountry.eys_f/20.7, $scope.selectedCountry.mys_f/13.1, $scope.selectedCountry.gni_f/59994],
+    ];
+      console.log($scope.selectedCountry.hdi_f)
+      return $scope.selectedCountry.name;
+    } else {
+      return "Please select an country";
+    }
+  };
 
   $scope.series = ['Male', 'Female'];
 
